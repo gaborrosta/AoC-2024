b63cc7d3dacaa4a99d4582e120464be5a8ff92c6
Rosta Gábor <rostagabor13@gmail.com>
2024-12-21 15:15:16 +0100

    Day 21

---
+++ src/Day21.kt
@@ -0,0 +1,20 @@
+fun main() {
+    val data = readFile(21)
+
+    println("Part 1: ${part1(data)}")
+    println("Part 2: ${part2(data)}")
+}
+
+
+//Part 1
+private fun part1(data: List<String>): Int {
+    //I did not manage to solve this on my own, I needed help from others' solutions.
+    return -1
+}
+
+
+//Part 2
+private fun part2(data: List<String>): Int {
+    //I did not manage to solve this on my own, I needed help from others' solutions.
+    return -1
+}
